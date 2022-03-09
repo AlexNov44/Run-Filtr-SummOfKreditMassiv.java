fbb92384033f87e5f54a1e413da694f1ca89fb10
Alexey <AlexNov8344@yandex.ru>
2022-03-09 22:25:20 +0300

    Initial commit

---
+++ src/banlKreditMassiv/Client.java
@@ -0,0 +1,34 @@
+package banlKreditMassiv;
+
+ public class Client {
+    String name;
+    String nameOfCredit;
+    int SummOfCredit;
+
+    { name="Неизвестный";
+       SummOfCredit=0;
+       nameOfCredit="Типовой";
+    }
+
+    Client(String name1) {
+       this.name=name1;
+    }
+
+    Client(String name1, String nameOfCredit1, int SummOfCredit1 ){
+       this.name=name1;
+       this.nameOfCredit=nameOfCredit1;
+       this.SummOfCredit=SummOfCredit1;;
+    }
+
+    Client(String name1,  int SummOfCredit1 ){
+       this.name=name1;
+       this.SummOfCredit=SummOfCredit1;;
+    }
+
+    Client(int i) {
+    }
+
+    Client() {
+    }
+
+}
---
+++ src/banlKreditMassiv/Main.java
@@ -0,0 +1,30 @@
+package banlKreditMassiv;
+
+public class Main {
+ static int x=4;
+    public static void main(String[] args) {
+
+
+        Client cli1 = new Client("Петр");
+        Client cli2 = new Client();
+        Client cli3 = new Client("Иван", 100);
+        Client cli4 = new Client("Игорь","Потребительский", 25);
+
+        Client Spisok[] = new Client[x];
+
+        Spisok[0] = cli1;
+        Spisok[1] = cli2;
+        Spisok[2] = cli3;
+        Spisok[3] = cli4;
+        for (int i = 0; i < 4; i++) {
+            if (Spisok[i].SummOfCredit >= 36) {
+                System.out.println(
+                        "Имя: " + Spisok[i].name +"/"+ "  Сумма кредита: "+
+                                  Spisok[i].SummOfCredit+" руб/"+"  Кредит: "+
+                                  Spisok[i].nameOfCredit);
+
+
+            }
+        }
+    }
+}
