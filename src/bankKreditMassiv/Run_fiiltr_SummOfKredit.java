ebc00025f0a97007b13d870930f7af8bae8269f2
Alexey <AlexNov8344@yandex.ru>
2022-03-12 17:10:02 +0300

    Massiv- cikl for

--- src/bankKreditMassiv/Run_fiiltr_SummOfKredit.java
+++ src/bankKreditMassiv/Run_fiiltr_SummOfKredit.java
@@ -1,7 +1,12 @@
-package banlKreditMassiv;
+package bankKreditMassiv;
 
-public class Main {
- static int x=4;
+import org.w3c.dom.ls.LSOutput;
+
+public class Run_fiiltr_SummOfKredit {
+ static int x=5;  //  Кол-во клиентов
+    static int filtr_SummOfKredit= 36; //величина, относительно которой
+                         // происходит фильтрация, если сумма кредита больше этой
+                         // величины, то распечатается имя клиента и название кредита
     public static void main(String[] args) {
 
 
@@ -9,7 +14,7 @@ public class Main {
         Client cli2 = new Client();
         Client cli3 = new Client("Иван", 100);
         Client cli4 = new Client("Игорь","Потребительский", 25);
-      //  Client cli5 = new Client("Павел","Авто", 200);
+        Client cli5 = new Client("Павел","Авто", 200);
 
         Client Spisok[] = new Client[x];
 
@@ -17,9 +22,10 @@ public class Main {
         Spisok[1] = cli2;
         Spisok[2] = cli3;
         Spisok[3] = cli4;
-      // Spisok[4] = cli5;
+        Spisok[4] = cli5;
+        System.out.println("-------------------");
         for (int i = 0; i < x; i++) {
-            if (Spisok[i].SummOfCredit >= 36) {
+            if (Spisok[i].SummOfCredit >= filtr_SummOfKredit) {
                 System.out.println(
                         "Имя: " + Spisok[i].name +"/"+ "  Сумма кредита: "+
                                   Spisok[i].SummOfCredit+" руб/"+"  Кредит: "+
